136f1eca8f9216273a6e947a8cd3030101e9ff7a
Matias <you@example.com>
2022-03-22 14:38:42 -0300

    first commit

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,27 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Crear(TipoCuenta tipocuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipocuenta);
+            }
+
+            return View();
+        }
+    }
+}
